c2212c1a19d725ce7b68902991064e69a25fdbe0
Claus Höfele <claus@claushoefele.com>
2018-02-26 17:33:24 +0100

    Add single view app template
    Deployment target: iOS 9
    Xcode 9.2

---
+++ TMDb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TMDb
+//
+//  Created by Claus Höfele on 26.02.18.
+//  Copyright © 2018 Claus Höfele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
